1898355a353882523e7609b86e1260cf54b90257
marvin <marvin23m@gmail.com>
2020-09-08 13:16:43 +0100

    Add color to the points based on elevation

--- map1.py
+++ map1.py
@@ -4,13 +4,22 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
-name = list(data["NAME"])
+elev = list(data["ELEV"])
+
+def colour_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln, nm in zip(lat, lon, name):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=nm, icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=el, icon=folium.Icon(color=colour_producer(el))))
 
 map.add_child(fg)
 
