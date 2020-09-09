ae1b9d5f5436312df1626cb3445e12d051a875d0
marvin <marvin23m@gmail.com>
2020-09-09 12:01:08 +0100

    Make map chloropleth

--- map1.py
+++ map1.py
@@ -14,14 +14,18 @@ def colour_producer(elevation):
     else:
         return 'red'
 
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=el, fill_color=colour_producer(el), color = 'grey', fill_opacity=0.7))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=el, fill_color=colour_producer(el),
+                                     color='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+                            style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else
+                            'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
 map.save("Map1.html")
